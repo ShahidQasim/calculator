7f8d29488ca062e9734b08657dfe531a611a6798
shahid <6shahid6@gmail.com>
2024-08-24 11:06:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    {
+        name: "firstnumber",
+        type: "number",
+        message: "enter first number"
+    },
+    {
+        name: "secondnumber",
+        type: "number",
+        message: "enter your second number"
+    },
+    {
+        name: "operator",
+        type: 'list',
+        message: "select your operator",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    }
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else {
+    console.log("enter correct number");
+}
